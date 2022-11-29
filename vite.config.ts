ae3455f4a39240f63e8c8b6dbd079896ef8ecb76
Arsenic-33 <omarahmed2441@gmail.com>
2022-11-29 20:16:41 +0200

    fixed config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/IP-ADDRESS-TRACKER",
+  base: "/IP-ADDRESS-TRACKER/",
   build: {
     minify: "esbuild",
     target: "esnext",
